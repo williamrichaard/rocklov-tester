738b1de2dd7122699d5a8e82ee183eddd26a937e
williamrichaard <wrichaard@hotmail.com>
2021-05-09 09:45:51 -0300

    Automatizando steps de cadastro

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -1,12 +1,13 @@
 Dado('que acesso a página de cadastro') do
     visit "http://rocklov-web:3000/signup"
-    sleep 10 #temporario
 end
   
 Quando('submeto o meu cadastro completo') do
     find("#fullName").set "William Richard"#método do capybara e através dele da para buscar elementos css
     find("#email").set "wrichaard@hotmail.com"
     find("#password").set "docker123"
+
+    click_button "Cadastrar"
 end
   
 Então('sou redirecionado para o Dashboard') do
