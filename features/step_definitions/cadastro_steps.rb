e226d04db9450de586e3a9bb6ea084b24148aca3
williamrichaard <wrichaard@hotmail.com>
2021-05-09 09:59:26 -0300

    Automatizando steps de dashboard

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -3,7 +3,7 @@ Dado('que acesso a página de cadastro') do
 end
   
 Quando('submeto o meu cadastro completo') do
-    find("#fullName").set "William Richard"#find método do capybara e através dele da para buscar elementos css
+    find("#fullName").set "William Richard" #find método do capybara e através dele da para buscar elementos css
     find("#email").set "wrichaard@hotmail.com"
     find("#password").set "docker123"
 
@@ -11,5 +11,6 @@ Quando('submeto o meu cadastro completo') do
 end
   
 Então('sou redirecionado para o Dashboard') do
-    expect(page).to have_css ".dashboard"#expect Recursos de validação do Rspec
+    expect(page).to have_css ".dashboard" #expect Recursos de validação do Rspec
+    sleep 10 # temporário
 end
